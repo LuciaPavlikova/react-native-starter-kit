8672c4696eb3bf867cc3fe5fd0bdafb476b2fd80
Lucia Pavlíková <33963381+LuciaPavlikova@users.noreply.github.com>
2018-03-11 18:44:50 +0100

    Update firebase.js

--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: 'AIzaSyAHshJehJ-E_9AoUN3kfY8BLvZtFoXxFto',
-  authDomain: 'react-native-starter-app.firebaseapp.com',
-  databaseURL: 'https://react-native-starter-app.firebaseio.com',
-  projectId: 'react-native-starter-app',
-  storageBucket: 'react-native-starter-app.appspot.com',
-  messagingSenderId: '762822550667',
+  apiKey: 'AIzaSyAxF6u3eTVJWT8hD3pNmp4aVUmywDXHL40',
+  authDomain: 'react-native-starter-app-2918e.firebaseapp.com',
+  databaseURL: 'https://react-native-starter-app-2918e.firebaseio.com/',
+  projectId: 'react-native-starter-app-2918e',
+  storageBucket: 'gs://react-native-starter-app-2918e.appspot.com/',
+  messagingSenderId: '284616120425',
 };
